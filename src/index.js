d0db10e0ee9c79a5ac0c718859d7376c983ce638
aditya <kumar.93@iitj.ac.in>
2020-06-16 16:43:55 +0530

    added azure

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import SocketManager from './SocketManager.js';
 window.base = 'http://localhost:8000';
 window.hostName = 'localhost:8000';
 window.media_url = window.base + '/media/';
+// window.media_url = 'https://eduhub.blob.core.windows.net/eduhub/';
 window.cred = 'include';
 
 ReactDOM.render(
