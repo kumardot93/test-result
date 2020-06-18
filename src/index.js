3136fdf7abdd4fc337966d41077fa18250bb0d16
aditya <kumar.93@iitj.ac.in>
2020-06-18 23:43:05 +0530

    production changes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import Store from './redux/Store.js';
 import { Provider } from 'react-redux';
 import SocketManager from './SocketManager.js';
 
-window.base = 'http://localhost:8000';
+window.base = '';
 // window.hostName = 'localhost:8000';
-window.media_url = window.base + '/media/';
-// window.media_url = 'https://eduhub.blob.core.windows.net/eduhub/';
-window.cred = 'include';
+// window.media_url = window.base + '/media/';
+window.media_url = 'https://eduhub.blob.core.windows.net/eduhub/';
+window.cred = 'same-origin';
 
 ReactDOM.render(
 	<Provider store={Store}>
